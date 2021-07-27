9d5a23e0f973c60213721e8978704c6e11a2b0e6
vorsakha <andreitf.dev@gmail.com>
2021-07-27 12:05:36 -0300

    quickie

---
+++ index.ts
@@ -0,0 +1,42 @@
+const { parse } = require("node-html-parser");
+const puppeteer = require("puppeteer");
+
+const url = "https://www.flashscore.com.br/equipe/flamengo/WjxY29qB/";
+
+const scrape = async () => {
+  try {
+    const browser = await puppeteer
+      .launch
+      // { headless: false }
+      ();
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    const data = await page.evaluate(() => document.body.innerHTML);
+
+    // await page.screenshot({ path: "site.png" });
+
+    await browser.close();
+
+    const dom = parse(data);
+
+    const homeTeam = dom.querySelector(".event__participant--home").textContent;
+    const awayTeam = dom.querySelector(".event__participant--away").textContent;
+
+    const score = dom.querySelector(".event__scores").querySelectorAll("span");
+
+    const scores = {
+      home: score[0].textContent,
+      away: score[1].textContent,
+    };
+
+    const lastGame = `${homeTeam} ${scores.home} x ${scores.away} ${awayTeam}`;
+
+    return lastGame;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+scrape().then(console.log);
